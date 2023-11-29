ebfaa634e61658c6926ac0452b4ef640aee67b7f
loicgalland <loicgalland1@gmail.com>
2023-11-29 10:41:56 +0100

    Correction of data routes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ export default function Contact(){
         <div className="section__contact">
             <h2 className="contact__title">Contact</h2>
             <p className="contact__email">
-                N’hésitez pas à m’envoyer un mail, je serais ravis d’échanger avec vous à propos de vos besoin !
+                N’hésitez pas à m’envoyer un mail, je serais ravis d’échanger avec vous à propos de vos besoins !
             </p>
             <a href="mailto: loicgalland1@gmail.com" className="link__text">loicgalland1@gmail.com</a>
             <p className="contact__phone">Si vous vous sentez plus à l’aise à l’oral,
