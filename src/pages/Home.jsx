dcb3712c4c32586efe0f012a03b9a79ce56dc492
loicgalland <loicgalland1@gmail.com>
2023-12-05 10:20:29 +0100

    New feature: add informations in about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,17 +55,30 @@ function Home() {
         </section>
         <section className="section__about" style={sectionStyle} id="about">
             <img src={Portrait} alt="portrait-Loic-Galland" className="about__image"/>
-            <h2 className="about__title">À propos</h2>
-            <p className="about__description">
-                Des bases solides en graphisme, combinées à une connaissance approfondie des technologies du développeur frontend
-                me permettent de vous proposer une solution personnalisée et précise pour votre site web.
-                Si vous souhaitez en savoir plus au sujet de mon parcours, je vous invite à consulter mon <a target="_blank" href="../assets/images/CV_LoïcGalland.pdf" className="link__text">curriculum vitae</a>
+            <div>
+                <h2 className="about__title">À propos</h2>
+                <p className="about__description">
+                    Des bases solides en graphisme, combinées à une connaissance approfondie des technologies du développeur frontend
+                    me permettent de vous proposer une solution personnalisée et précise pour votre site web.
+                    Si vous souhaitez en savoir plus au sujet de mon parcours, je vous invite à consulter mon <a target="_blank" href="../assets/images/CV_LoïcGalland.pdf" className="link__text">curriculum vitae</a>
 
-            </p>
-            <p>
-                Vous avez un projet et vous voulez en parler c’est par <Link to="/contact" className="link__text">ici</Link>
-            </p>
-            <h2 className="about__skills__title">Compétences</h2>
+                </p>
+                <p>
+                    Vous avez un projet et vous voulez en parler c’est par <Link to="/contact" className="link__text">ici</Link>
+                </p>
+            </div>
+
+            <div className="container__experiences">
+                <h2>Mon parcours</h2>
+                <div className="experience__items">
+                    <span>2014-2016 - <span className="strong">BTS design graphique </span>(ESAAB, Nevers, 58)</span>
+                    <span>2016-2018 - <span className="strong">DSAA mention Graphisme</span> (ESAAB, Nevers, 58)</span>
+                    <span>09/2018-03/2019 <span className="strong">Échange Universitaire </span>(Suzhou Art and Design School, Chine)</span>
+                    <span>2019-2023 - <span className="strong">Infographiste</span> (Manhattan, Marcigny, 71)</span>
+                    <span>09/2023-12/2023 - <span className="strong">Formation Développeur web</span> (Openclassroom) </span>
+                </div>
+            </div>
+            <h2 className="about__skills__title">Mes compétences</h2>
             <div className="about__skills--logos">
                 {/* -- Call the map function on all the skill in JSON file -- */}
                 {Skills.map((skill, index) => (
